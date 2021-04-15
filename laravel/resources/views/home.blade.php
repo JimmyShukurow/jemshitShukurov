f7b05a23049934a32787d1c5d8602884a706f63a
jimishukurow <jimishukurow@gmail.com>
2021-04-15 23:01:06 +0300

    pageOne started

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -4,10 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="home.css">
     <title>Jemshit Shukurov</title>
+   
 </head>
-<body style="background-color: #ff0000">
-    Selam World!
-    
-</body>
+    <body>
+        <div class="pageOne">
+            <div class="header">
+                <h1 class="center">About me</h1>
+                <span class="fullname">
+                    <span class="name">JEMSHIT SHUKUROV</span>
+                    <span class="profession">Fullstack Developer</span>
+                    <img class="profileImage" src="/images/JimiSquare.jpg" alt="JemshitShukurov" width="200" height="200">
+                </span>  
+            </div>
+            
+            <div class="details">
+                <p>Birth Date: 11 Aprel 1990</p>
+                <p>Birth country: Tukmenistan</p>
+                <p>Birth City: Lebap / Seydi</p>
+            </div>
+            
+        </div>
+        <div class="pageTwo">
+            <h1 class="center">Education</h1>
+        </div>
+        <div class="pageThree">
+            <h1 class="center">Experience</h1>
+        </div>
+    </body>
 </html>
