c80623f1df0ccfa95f252c64b5cbd6727ed74a50
jimishukurow <jimishukurow@gmail.com>
2021-04-16 04:06:56 +0300

    animation is woking fine

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -5,32 +5,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="home.css">
+    <script src="jquery.js"></script>
+    <script src="animation.js"></script>
     <title>Jemshit Shukurov</title>
    
 </head>
     <body>
-        <div class="pageOne">
-            <div class="header">
-                <h1 class="center">About me</h1>
-                <span class="fullname">
-                    <span class="name">JEMSHIT SHUKUROV</span>
-                    <span class="profession">Fullstack Developer</span>
-                    <img class="profileImage" src="/images/JimiSquare.jpg" alt="JemshitShukurov" width="200" height="200">
-                </span>  
+            <div class="pageOne" id="firstPage">
+                    <h1 class="center">About me</h1>
+                    <span class="fullname">
+                        <span class="name">JEMSHIT SHUKUROV</span>
+                        <span class="profession">Fullstack Developer</span>
+                        <img class="profileImage" src="/images/JimiSquare.jpg" alt="JemshitShukurov" width="200" height="200">
+                    </span>  
+                
+                <div class="details">
+                    <p>Birth Date: 11 Aprel 1990</p>
+                    <p>Birth country: Tukmenistan</p>
+                    <p>Birth City: Lebap / Seydi</p>
+                    <p>Nationality: Turkmen</p>
+                </div>
+                <div class="hobbies-and-interests">
+                    I am physics and math loving guy.  Since when i was a boy 
+                    I had an interest to physics and math. I have learned physics and
+                    particapated to international olympiads. I am very curious in many 
+                    fileds like physics math computer sciense, robotics, 
+                    animations modeling and chess. 
+                </div>
+              
+                
             </div>
-            
-            <div class="details">
-                <p>Birth Date: 11 Aprel 1990</p>
-                <p>Birth country: Tukmenistan</p>
-                <p>Birth City: Lebap / Seydi</p>
+            <div class="pageTwo" id="secondPage">
+                <h1 class="center">Education</h1>
             </div>
-            
-        </div>
-        <div class="pageTwo">
-            <h1 class="center">Education</h1>
-        </div>
-        <div class="pageThree">
-            <h1 class="center">Experience</h1>
-        </div>
+            <div class="pageThree" id="thirdPage">
+                <h1 class="center">Experience</h1>
+            </div>
+        <button class="nextButton">Next ></button>
     </body>
 </html>
